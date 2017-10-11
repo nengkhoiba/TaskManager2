49b52bac6971d424c872df2e055822f00320da9d
nengkhoiba <Nengkhoi@macbook.local>
2017-10-11 19:47:45 +0530

    Initial commit

---
+++ app/src/main/java/com/taskmanager/AddTask.java
@@ -0,0 +1,44 @@
+package com.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class AddTask extends AppCompatActivity {
+
+    EditText task,details,summary;
+    Button save;
+    ArrayList<TaskItem> aTask=new ArrayList<TaskItem>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        task=(EditText) findViewById(R.id.edt_Task);
+        details=(EditText) findViewById(R.id.edt_details);
+        summary=(EditText) findViewById(R.id.edt_summary);
+        save=(Button) findViewById(R.id.btn_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskItem tItem=new TaskItem();
+
+                tItem.Task=task.getText().toString();
+                tItem.Details=details.getText().toString();
+                tItem.Summary=summary.getText().toString();
+                aTask.add(tItem);
+                Toast.makeText(AddTask.this,"Succesfully saved",Toast.LENGTH_LONG).show();
+                task.setText("");
+                details.setText("");
+                summary.setText("");
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/taskmanager/TaskItem.java
@@ -0,0 +1,12 @@
+package com.taskmanager;
+
+/**
+ * Created by Nengkhoi on 1/9/2017.
+ */
+
+public class TaskItem {
+    String Task;
+    String Details;
+    String Summary;
+
+}
